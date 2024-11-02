0047fc39ee0f5cd62cea5e9518742aca63e5666a
Onder Bakirtas <onderbakirtas@gmail.com>
2024-11-02 17:57:45 +0300

    optimized analytics script

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://buhartr.com',
+  integrations: [partytown()],
 });
