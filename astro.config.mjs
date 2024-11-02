fcbb3c2306dece683920e6718b782b45b26f461f
Onder Bakirtas <onderbakirtas@gmail.com>
2024-11-03 02:49:38 +0300

    simplified it further

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import partytown from "@astrojs/partytown";
+// import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://buhartr.com',
-  integrations: [partytown()],
+  // integrations: [partytown()],
 });
